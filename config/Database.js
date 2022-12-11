a33f4c08a9fe267ebf9126c02172bad34969cdf1
daffa-z <77871074+daffa-z@users.noreply.github.com>
2022-12-11 07:40:20 +0700

    db config

--- config/Database.js
+++ config/Database.js
@@ -1,8 +1,9 @@
 import { Sequelize} from "sequelize";
 
-const db = new Sequelize ('auth_db', 'root', '', {
-    host: "localhost",
+const db = new Sequelize ('railway', 'root', 'd8yFhl61Z7q2Sljd3kb5', {
+    host: "containers-us-west-140.railway.app",
     dialect: "mysql",
+    port: "6881",
 });
 
 export default db;
